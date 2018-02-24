49a268baf1013f5a55b938e9a8aeb0c34c0b56aa
Keith Wong <keith.wong3672@gmail.com>
2018-02-23 16:03:29 -0800

    Create component to display list of businesses

--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 
+class Business extends Component {
+  render() {
+    const address = this.props.location.display_address.join(' ');
+    return (
+      <div>
+        <div>Name: {this.props.name}</div>
+        <div>Rating: {this.props.rating}</div>
+        <div>Number of Reviews: {this.props.reviewCount}</div>
+        <div>Address: {address}</div>
+      </div>
+    )
+  }
+
+}
+
 class List extends Component {
 
   render() {
     return (
       <div>
         <h3>List of Physical Therapists</h3>
+        {this.props.businesses.map(business => 
+          <Business 
+            key={business.id} 
+            name={business.name} 
+            rating={business.rating}
+            reviewCount={business.review_count}
+            location={business.location} />
+        )}
       </div>
     );
   }
